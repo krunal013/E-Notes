8ce7567c868d198399a514ff9224ee395aef98eb
Krunal Mistry <krunalmistry2510@gmail.com>
2024-05-10 21:17:44 +0530

    First Node App

---
+++ ind.js
@@ -0,0 +1,62 @@
+//require some packages for our app
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+//setting up middleware for our app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+//middleware finished
+
+//dynamic routing
+app.get("/", function (req, res) {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", function (req, res) {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split("").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+//port handling
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
